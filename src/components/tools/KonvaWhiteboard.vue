1ca9d89d21d7b37cfa9d65d7af8165e2301a9213
wfei <wfei@foxhis.com>
2025-09-26 11:59:21 +0800

    xiufu

---
+++ src/components/tools/KonvaWhiteboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="konva-whiteboard">
+    <h2>Konva 白板</h2>
+    <p>此功能正在开发中...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KonvaWhiteboard'
+}
+</script>
+
+<style scoped>
+.konva-whiteboard {
+  padding: 20px;
+  text-align: center;
+}
+</style>
